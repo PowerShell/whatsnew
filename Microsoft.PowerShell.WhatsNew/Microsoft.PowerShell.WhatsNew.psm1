86e566e9bcd6961cf9dea36930a4d629e9bf94e3
Sean Wheeler <sean.wheeler@microsoft.com>
2022-06-22 13:02:27 -0500

    Simplify parameter sets

--- Microsoft.PowerShell.WhatsNew/Microsoft.PowerShell.WhatsNew.psm1
+++ Microsoft.PowerShell.WhatsNew/Microsoft.PowerShell.WhatsNew.psm1
@@ -1,7 +1,7 @@
 ﻿
-# This will return the available versions found on the disk.
-# Optionally, it will construct the objects used in the script
-# to display the what's new document.
+# This function returns the available versions found on the disk.
+# Optionally, it constructs the objects used in the script to display
+# the what's new document.
 function Get-AvailableVersion {
     param ( [switch]$urihashtable )
 
@@ -19,8 +19,9 @@ function Get-AvailableVersion {
             $fileVersion = $version -replace "\."
             if ( $fileVersion -eq "51" ) {
                 $fileBase = "What-s-New-in-Windows-PowerShell-50"
-            }
-            else {
+            } elseif ( $fileVersion -like "6*" ) {
+                $fileBase = "${filenameBase}-Core-${fileVersion}"
+            } else {
                 $fileBase = "${filenameBase}-${fileVersion}"
             }
             @{
@@ -36,6 +37,16 @@ function Get-AvailableVersion {
     }
 }
 
+function TestVersion {
+    param ( [string[]]$versions )
+    $allowedVersions = Get-AvailableVersion
+    foreach ($version in $versions) {
+        if ( $allowedVersions -notcontains $version ) {
+            throw ("$version not in: " + ( $allowedVersions -join ", "))
+        }
+    }
+    return $true
+}
 <#
     .SYNOPSIS
     Displays release notes for a version of PowerShell.
@@ -47,6 +58,9 @@ function Get-AvailableVersion {
 
     The cmdlet can display release notes for the following versions of PowerShell
     - Windows PowerShell 5.1
+    - PowerShell 6.0
+    - PowerShell 6.1
+    - PowerShell 6.2
     - PowerShell 7.0
     - PowerShell 7.1
     - PowerShell 7.2
@@ -66,9 +80,9 @@ function Get-AvailableVersion {
     Displays the release notes for PowerShell 5.1 regardless of which version the cmdlet is running.
 
     .EXAMPLE
-    Get-WhatsNew -Daily -Version 7.2
+    Get-WhatsNew -Daily -Version 7.0, 7.1, 7.2
 
-    Displays one randomly selected section of the release notes for PowerShell 7.2.
+    Displays one randomly selected section of the release notes per version of PowerShell selected.
 
     .EXAMPLE
     Get-WhatsNew -All
@@ -82,63 +96,54 @@ function Get-AvailableVersion {
     notes. If no version is specified, it uses the current version.
 
     .EXAMPLE
-    Get-WhatsNew -Version 7.2 -EndVersion 5.1
+    Get-WhatsNew -Version 7.0, 7.1, 7.2
 
-    Displays the release notes for PowerShell 5.1 through PowerShell 7.2. The order of the values
-    for parameters does not matter. Use this when you want to see what has change over a range of
-    versions.
+    Displays the release notes for PowerShell 7.0 through PowerShell 7.2.
 #>
 function Get-WhatsNew {
     [CmdletBinding(DefaultParameterSetName = 'ByVersion')]
     param (
         # The version number of PowerShell to be displayed. If not specified, the current version is used.
         [Parameter(Position=0,ParameterSetName='ByVersion')]
-        [Parameter(Position=0,ParameterSetName='ByVersionRange')]
+        [Parameter(Position=0,ParameterSetName='ByVersionDaily')]
+        [Parameter(Position=0,ParameterSetName='ByVersionOnline')]
         [ValidateScript({TestVersion $_})]
-        [string]$Version,
-
-        # The version number of PowerShell used to defined the range of versions to be displayed.
-        [Parameter(Mandatory,ParameterSetName='ByVersionRange')]
-        [ValidateScript({TestVersion $_})]
-        [string]$EndVersion,
-
-        # Displays release notes for all versions.
-        [Parameter(Mandatory,ParameterSetName='AllVersions')]
-        [switch]$All,
+        [string[]]$Version,
 
         # Dislays a single section of the releases for a version. Alias = `MOTD`.
-        [Parameter(Position=0,ParameterSetName='ByVersion')]
+        [Parameter(Mandatory,ParameterSetName='ByVersionDaily')]
         [Alias('MOTD')]
         [switch]$Daily,
 
         # Takes you to the release notes webpage for the specified version.
-        [Parameter(ParameterSetName='ByVersion')]
-        [switch]$Online
+        [Parameter(Mandatory,ParameterSetName='ByVersionOnline')]
+        [switch]$Online,
+
+        # Displays release notes for all versions.
+        [Parameter(Mandatory,ParameterSetName='AllVersions')]
+        [switch]$All
     )
 
-    $versions = Get-AvailableVersion -uriHashtable
-
-    if (0 -eq $Version) {
-        $Version = [double]('{0}.{1}' -f $PSVersionTable.PSVersion.Major,$PSVersionTable.PSVersion.Minor)
+    if ($Version.Count -eq 0) {
+        $Version = '{0}.{1}' -f $PSVersionTable.PSVersion.Major, $PSVersionTable.PSVersion.Minor
     }
+
+    $versions = Get-AvailableVersion -uriHashtable | Where-Object {$_.version -in $Version}
 
     # Resolve parameter set
     $mdfiles = @()
-    if ($PsCmdlet.ParameterSetName -eq 'EndVersion') {
-        if ($Version -gt $EndVersion) {
-            $tempver = $EndVersion
-            $EndVersion = $Version
-            $Version = $tempver
-        }
-        foreach ($ver in $versions) {
-            if (($ver.version -ge $Version) -and ($ver.version -le $EndVersion)) {
-                $mdfiles += $ver.path
-            }
-        }
-    } elseif ($PsCmdlet.ParameterSetName -eq 'AllVersions') {
+    if ($PsCmdlet.ParameterSetName -eq 'AllVersions') {
         $mdfiles = ($versions).path
     } else {
-        $mdfiles = ($versions | Where-Object version -eq $Version).path
+        $mdfiles = ($versions | Where-Object {$_.version -in $Version}).path
+    }
+
+    if ($PsCmdlet.ParameterSetName -eq 'ByVersionOnline') {
+        if ($Version.Count -gt 1) {
+            Write-Warning 'This -Online parameter only supports one value for -Version. Using first value.'
+        }
+        Start-Process ($versions | Where-Object {$_.version -in $Version[0]}).url
+        return
     }
 
     # Scan release notes for H2 blocks
@@ -147,9 +152,8 @@ function Get-WhatsNew {
         $mdtext = Get-Content $file -Encoding utf8
         $mdheaders = Select-String -Pattern '^##\s',$endMarker -Path $file
 
-        $blocklist = @()
-
         ## Build a list of H2 blocks
+        $blocklist = @()
         foreach ($hdr in $mdheaders) {
             if ($hdr.Line -ne $endMarker) {
                 $block = @{
@@ -170,11 +174,15 @@ function Get-WhatsNew {
             }
         }
 
+        if ( $file[-5] -eq '5') {
+            $fileVersion = '5.1'
+        } else {
+            $fileVersion = '{0}.{1}' -f $file[-5], $file[-4]
+        }
+        '# Release notes for PowerShell {0}{1}' -f $fileVersion, [System.Environment]::NewLine
         if ($Daily) {
             $block = $blocklist | Get-Random -SetSeed (get-date -UFormat '%s')
             $mdtext[$block.StartLine..$block.EndLine]
-        } elseif ($Online) {
-            Start-Process ($versions | Where-Object version -eq $Version).url
         } else {
             foreach ($block in $blocklist) {
                 $mdtext[$block.StartLine..$block.EndLine]
@@ -182,3 +190,9 @@ function Get-WhatsNew {
         }
     }
 }
+
+$sbVersions = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    Get-AvailableVersion | Where-Object {$_ -like "$wordToComplete*"}
+}
+Register-ArgumentCompleter -CommandName Get-WhatsNew -ParameterName Version -ScriptBlock $sbVersions
