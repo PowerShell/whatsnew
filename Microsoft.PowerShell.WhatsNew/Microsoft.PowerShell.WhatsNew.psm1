e09a21b498c457b0ec074ec7c5bc1ff9f48871bc
Sean Wheeler <sean.wheeler@microsoft.com>
2024-06-19 08:24:28 -0500

    Update release notes

--- Microsoft.PowerShell.WhatsNew/Microsoft.PowerShell.WhatsNew.psm1
+++ Microsoft.PowerShell.WhatsNew/Microsoft.PowerShell.WhatsNew.psm1
@@ -68,7 +68,8 @@ function TestVersion {
     - PowerShell 7.1
     - PowerShell 7.2
     - PowerShell 7.3
-    - PowerShell 7.4 (preview)
+    - PowerShell 7.4
+    - PowerShell 7.5 (preview)
 
     By default, the cmdlet shows all of the release notes for a version. You can also limit it to
     display a single random section of the release notes. This can be used as a "Message of the Day".
